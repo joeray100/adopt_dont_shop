b9c0e5f0da3bbc8b9fc1f9f5bbd465cbe9cac6b1
Joe Ray <jray100.jr@gmail.com>
2021-05-23 01:24:51 -0600

    Update enum in model

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,7 +2,7 @@ class Application < ApplicationRecord
   has_many :pet_applications, dependent: :destroy
   has_many :pets, through: :pet_applications
 
-  enum status: [:"in_progress", :"pending", :"approved", :"rejected"]
+  enum status: {"In Progress": 0, "Pending": 1, "Approved": 2, "Rejected": 3}
 
   validates :name, :address, :city, :state, :description, :status, presence: true
   validates :zip, presence: true, numericality: true
