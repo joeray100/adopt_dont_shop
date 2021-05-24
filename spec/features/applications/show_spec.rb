30b96f8887760c7c64a2f4e07b5d16255d6e786a
Joe Ray <jray100.jr@gmail.com>
2021-05-23 20:16:39 -0600

    Add test for submitting app and if you have no pets

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -16,29 +16,23 @@ RSpec.describe 'Applications Show Page' do
     @shelter2 = Shelter.create!(name: "What Animal Rescue", city: "Boston", rank: 4, foster_program: false)
     @shelter3 = Shelter.create!(name: "Boss Animal Rescue", city: "Chicago", rank: 9, foster_program: true)
 
-    @pet1 = @shelter1.pets.create!(name: "Babushka", breed: "Corgi", age: 70)
-    @pet2 = @shelter1.pets.create!(name: "Fifi", breed: "St. Bernard", age: 3)
-    @pet3 = @shelter1.pets.create!(name: "Myrtle", breed: "Maine Coon", age: 11)
+    @pet1 = @shelter1.pets.create!(name: "Babushka", breed: "Corgi", age: 70, adoptable: true)
+    @pet2 = @shelter1.pets.create!(name: "Fifi", breed: "St. Bernard", age: 3, adoptable: true)
+    @pet3 = @shelter1.pets.create!(name: "Myrtle", breed: "Maine Coon", age: 11, adoptable: true)
 
-    @pet4 = @shelter2.pets.create!(name: "Skittles", breed: "Ragdoll", age: 9)
-    @pet5 = @shelter2.pets.create!(name: "Vixen", breed: "Pointer", age: 2)
-    @pet6 = @shelter2.pets.create!(name: "Dino", breed: "Toy Poodle", age: 14)
+    @pet4 = @shelter2.pets.create!(name: "Skittles", breed: "Ragdoll", age: 9, adoptable: true)
+    @pet5 = @shelter2.pets.create!(name: "Vixen", breed: "Pointer", age: 2, adoptable: true)
+    @pet6 = @shelter2.pets.create!(name: "Dino", breed: "Toy Poodle", age: 14, adoptable: true)
 
-    @pet7 = @shelter3.pets.create!(name: "Buttercup", breed: "Sphynx", age: 6)
-    @pet8 = @shelter3.pets.create!(name: "OJ", breed: "Bombay", age: 10)
-    @pet9 = @shelter3.pets.create!(name: "Stewie", breed: "French Bulldog", age: 4)
+    @pet7 = @shelter3.pets.create!(name: "Buttercup", breed: "Sphynx", age: 6, adoptable: true)
+    @pet8 = @shelter3.pets.create!(name: "OJ", breed: "Bombay", age: 10, adoptable: true)
+    @pet9 = @shelter3.pets.create!(name: "Stewie", breed: "French Bulldog", age: 4, adoptable: true)
 
     @application1 = Application.create!(name: "Sketchy Steve", address: "6107 Dudley Ct", city: "Winchester", state: "NC", zip: 51631, description: "wide open spaces", status: 0)
-    @application2 = Application.create!(name: "Jill", address: "31 Columbus Dr", city: "Denver", state: "CO", zip: 62814, description: "animals LOVE me", status: 1)
+    @application2 = Application.create!(name: "Jill", address: "31 Columbus Dr", city: "Denver", state: "CO", zip: 62814, description: "animals LOVE me", status: 0)
     @application3 = Application.create!(name: "Thadious", address: "6 Pickle St", city: "york", state: "OH", zip: 51631, description: "I have food...sometimes", status: 0)
-    @application4 = Application.create!(name: "Yattle", address: "1123 Tickle Ct", city: "Minster", state: "CO", zip: 36297, description: "woof", status: 3)
+    @application4 = Application.create!(name: "Yattle", address: "1123 Tickle Ct", city: "Minster", state: "CO", zip: 36297, description: "woof", status: 0)
     @application5 = Application.create!(name: "Sarah", address: "92 Ball Dr", city: "Arvada", state: "CO", zip: 36419, description: "I have yarn and squeeky toys", status: 0)
-
-    @application1.pets.push(@pet1, @pet6)
-    @application2.pets.push(@pet2)
-    @application3.pets.push(@pet3)
-    @application4.pets.push(@pet4)
-    @application5.pets.push(@pet5)
 
     visit application_path(@application1)
   end
@@ -48,8 +42,8 @@ RSpec.describe 'Applications Show Page' do
     expect(page).to have_content(@application1.address)
     expect(page).to have_content(@application1.city)
     expect(page).to have_content(@application1.state)
-    expect(page).to have_content(@application1.description)
     expect(page).to have_content(@application1.status)
+    expect(page).to have_content(@application1.description)
 
     expect(page).to_not have_content(@application2.name)
   end
@@ -74,7 +68,7 @@ RSpec.describe 'Applications Show Page' do
     fill_in 'Search', with: 'myrtle'
 
     click_on 'Submit'
-    
+
     expect(page).to have_button('Adopt this Pet')
 
     click_on 'Adopt this Pet'
@@ -84,5 +78,33 @@ RSpec.describe 'Applications Show Page' do
     expect(page).to have_content(@pet3.name)
     expect(page).to have_content(@pet3.age)
     expect(page).to have_content(@pet3.breed)
+  end
+
+  describe "In the section to submit my application, I see an area to enter why I would make a good owner for these pet(s)." do
+    it "When I fill this area in and click submit, on the same page, I see the status is pending and all of the pets I want to adopt. I do not see a section to add more pets" do
+      visit "/applications/#{@application1.id}?adopt=#{@pet3.id}"
+
+      expect(current_path).to eq(application_path(@application1))
+      expect(page).to have_content('Why would you make a good owner for these pet(s)?')
+      expect(page).to have_content('Submit Application')
+
+      fill_in 'application_reason', with: "I have wide open spaces"
+      click_on 'Submit Application'
+
+      expect(current_path).to eq(application_path(@application1))
+      expect(page).to have_content('Status - Pending')
+      expect(page).to have_content(@pet3.name)
+      expect(page).to have_content(@pet3.age)
+      expect(page).to have_content(@pet3.breed)
+      
+      expect(page).to_not have_content('Submit Application')
+      expect(page).to_not have_button('Adopt this Pet')
+      expect(page).to_not have_content('Why would you make a good owner for these pet(s)?')
+    end
+  end
+
+  it "If I have not added any pets to the application, then I do not see a section to submit my application" do
+    expect(@application1.pet_count).to eq(0)
+    expect(page).to_not have_content('Submit Application')
   end
 end
