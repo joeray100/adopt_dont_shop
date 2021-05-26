f97d9add3a82e4898e4a24358e4e99d940fad146
Joe Ray <jray100.jr@gmail.com>
2021-05-26 07:49:40 -0600

    Merge pull request #36 from joeray100/shelters-pending-app
    shelters-pending-app - complete

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,5 +1,6 @@
 class Admin::SheltersController < ApplicationController
   def index
     @shelters = Shelter.reverse_order_by_name
+    @pending_app_shelters = Shelter.shelters_with_pending_apps
   end
 end
