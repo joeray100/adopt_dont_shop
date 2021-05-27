478f03343f9e3c85beaff12a28d7ff8ce1b1873d
Joe Ray <jray100.jr@gmail.com>
2021-05-26 19:20:35 -0600

    Updated controller to reflect flash notice for error in new app form

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -18,7 +18,7 @@ class ApplicationsController < ApplicationController
     if application.save
       redirect_to "/applications/#{application.id}"
     else
-      flash[:notice] = "ERROR: Missing required information!"
+      flash[:notice] = "Application not created: #{application.errors.full_messages.to_sentence}"
       render :new
     end
   end
